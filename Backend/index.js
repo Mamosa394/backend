0f6d146cd778fd7d6e42b4b6e43e3b269aeeff1f
Mamosa394 <motsiemamosa@gmail.com>
2025-05-12 13:50:17 -0700

    index

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
-  "https://iwb-deploy-p4ea.vercel.app",
+  "https://iwb-deployment-7a7d.vercel.app",
 ];
 
 // ✅ CORS Middleware (must be before routes and json parsing)
